59fb46916fdc55cec0fa5bbee596f8bafd4ff332
Caisin <kaixing622@qq.com>
2022-10-22 10:13:48 +0800

    新增utc8时间转换

--- src/times.rs
+++ src/times.rs
@@ -1,12 +1,26 @@
 use std::str::FromStr;
 
-use chrono::{DateTime, Local, NaiveDateTime, Utc};
+use chrono::{DateTime, FixedOffset, Local, NaiveDateTime, Utc};
 
 pub static YMD: &str = "%Y%m%d";
 pub static YMD_PATH: &str = "%Y/%m/%d";
 pub static YMD_M: &str = "%Y-%m-%d";
 pub static DATE_TIME_M: &str = "%Y-%m-%d  %H:%M:%S";
 pub static YMDH_PATH: &str = "%Y/%m/%d/%H";
+
+/// unix时间转datetime
+pub fn unix_str_2_local_date_time(s: &str) -> DateTime<Local> {
+    let secs = i64::from_str(s).unwrap();
+    unix_2_local_date_time(secs)
+}
+
+/// unix时间转datetime
+pub fn unix_2_local_date_time(s: i64) -> DateTime<Local> {
+    let naive_datetime = NaiveDateTime::from_timestamp(s, 0);
+    let east = FixedOffset::east(8 * 3600);
+    let datetime: DateTime<Local> = DateTime::from_utc(naive_datetime, east);
+    datetime
+}
 
 /// unix时间转datetime
 pub fn unix_str_2_date_time(s: &str) -> DateTime<Utc> {
@@ -65,4 +79,14 @@ pub fn get_yesterday() -> DateTime<Utc> {
 pub fn sys_date_time() -> DateTime<Utc> {
     let naive_utc = Local::now().naive_utc();
     DateTime::from_utc(naive_utc, Utc)
+}
+
+#[test]
+fn test_time() {
+    // let now = Local::now();
+    // let timestamp = now.timestamp();
+    let timestamp = 1666404492;
+    let u = unix_2_date_time(timestamp);
+    let l = unix_2_local_date_time(timestamp);
+    println!("{:?}\n{:?}", u, l);
 }
